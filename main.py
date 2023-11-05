db62966bb7e90cdecb76279f291737148ab6ed18
Pavel <pavel.stanoev1997@gmail.com>
2023-11-05 14:13:38 +0200

    total_time_fucntion

--- main.py
+++ main.py
@@ -18,12 +18,26 @@ def parse_courage_cards_json(file_path: str) -> pd.DataFrame:
             event_df = pd.DataFrame(data['event'].items(), columns=['event_id', 'event'])
             created_df = pd.DataFrame(data['created'].items(), columns=['event_id', 'timestamp'])
             df = pd.merge(event_df, created_df, on='event_id')
+            df['timestamp'] = pd.to_datetime(df['timestamp'], format='ISO8601')
             return df
     except FileNotFoundError:
         raise FileNotFoundError(f"The file at {file_path} was not found.")
     except json.JSONDecodeError:
         raise ValueError("Couldn't decode the file")
     except Exception as e:
-        raise Exception(f"An unexpected error occurred: {e}")
+        raise e
 
+
+def total_time_spent(df: pd.DataFrame, timestamp_col: str) -> float:
+    """
+    Calculates the total time in seconds between the first and last entries in the timestamp column.
+
+    :param df: Dataframe containing timestamp_col
+    :param timestamp_col: string name of the column that contains timestamp info
+    :return: float representation of the time between first and last timestamps
+    """
+    total_time = df[timestamp_col].iloc[-1] - df[timestamp_col].iloc[0]
+    total_seconds = total_time.total_seconds()
+
+    return total_seconds
 
